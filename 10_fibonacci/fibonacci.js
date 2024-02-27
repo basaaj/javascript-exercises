f231f349f8ae02ac589a6cf8322256f43c359558
j <93398891+basaaj@users.noreply.github.com>
2024-02-26 20:43:15 -0600

    Complete lesson 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let theNum = Number(num);
 
+    if (theNum === 0) return 0;
+    if (theNum < 0) return "OOPS";
+
+    let currNum = 1;
+    let prevNum = 0;
+
+    for (let i = 2; i <= theNum; i++) {
+        let sum = prevNum + currNum;
+        prevNum = currNum;
+        currNum = sum;
+    }
+
+    return currNum;
 };
 
 // Do not edit below this line
